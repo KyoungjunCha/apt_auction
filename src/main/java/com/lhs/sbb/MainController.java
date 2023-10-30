f7af101f9622f8c027882218b6327b9f8e97f64c
cha <zktmzkxh1234@gmail.com>
2023-10-30 10:12:19 +0900

    23.10.30 10:12

--- src/main/java/com/lhs/sbb/MainController.java
+++ src/main/java/com/lhs/sbb/MainController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping()
 public class MainController {
     
-    @GetMapping("/index1-1")
+    @GetMapping("/index")
     public String index() {
         return "index_intro";
     }
